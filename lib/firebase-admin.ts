2b231c88a1e01cdcb4fdd9c2ff9d9d8fb581369c
Prakash-56 <prakash.connect06@gmail.com>
2026-03-08 16:13:58 +0530

    Version 1.4

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,6 +1,11 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./firebase-service-account.json";
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 if (!getApps().length) {
   initializeApp({
